247681cbbbb7eda8b25145a17d89e6bf70872a7d
Archer Goulding <vortexcraft.archer@gmail.com>
2026-01-14 13:18:46 -0600

    Completed prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int secretNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        while(guess != secretNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if(guess < secretNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(guess > secretNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
