f8af8e0a2b2d59f716935e50f3e865316d9586f9
Archer Goulding <vortexcraft.archer@gmail.com>
2026-01-14 16:57:46 -0600

    Completed prep5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
+        static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("What is your birth year? ");
+        year = int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string name, int squareNumber, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squareNumber}");
+        int age = 2026 - birthYear;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int year;
+        PromptUserBirthYear(out year);
+        int squareNumber = SquareNumber(number);
+        DisplayResult(name, squareNumber, year);
+
     }
 }
