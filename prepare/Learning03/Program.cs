6113c7a552920003efcb9fde95ccef5c7290db70
Archer Goulding <vortexcraft.archer@gmail.com>
2026-01-21 22:42:35 -0600

    Completed Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Random randomGenerator = new Random();
+        for (int i = 1; i < 21; i++)
+        {
+            fraction1.SetTop(randomGenerator.Next(1, 100));
+            fraction1.SetBottom(randomGenerator.Next(1, 100));
+            Console.WriteLine($"Fraction {i}: string: {fraction1.GetFractionString()} Number: {fraction1.GetDecimalValue()}");
+        }
     }
+
 }
