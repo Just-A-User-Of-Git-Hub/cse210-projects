5c5044f09a6d61887f803e4bbc244ab3f5a108ea
Archer Goulding <vortexcraft.archer@gmail.com>
2026-02-02 12:40:21 -0600

    Completed Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 10));
+        shapes.Add(new Rectangle("blue", 10, 5));
+        shapes.Add(new Circle("green", 5));
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
